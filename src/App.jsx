8587ffafdddf90b13371ca378c7e54d010286152
Andres_Prado_Morgaz <andres.prado.external@eviden.com>
2024-10-01 18:32:45 +0200

    Real basic routing using state and conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import { useState } from 'react'
 import './App.css'
 
-function App() {
-
+function HomePage() {
   return (
     <>
+      <h1>Home</h1>
+      <p>Example page to create a react router from scratch.</p>
+      <a href='/about'>About us</a>
+    </>
+  )
+}
+
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <div>
+        <img src='/profilePic.webp' width='250px' alt='Andrés profile picture' />
+        <p>Hi! I&apos;m Andrés, and I&apos;m creating a React Router clone.</p>
+      </div>
+      <a href='/'>Home</a>
+    </>
+  )
+}
+
+function App() {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname)
+  return (
+    <>
+      <main>
+        {currentPath === '/' && <HomePage />}
+        {currentPath === '/about' && <AboutPage />}
+      </main>
     </>
   )
 }
