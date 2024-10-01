3cb5b0b108d72b35a6b7900c3e84cc51f41cc55a
Andres_Prado_Morgaz <andres.prado.external@eviden.com>
2024-10-01 19:01:51 +0200

    Implemented navigate method.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 In this project I'll attempt to make my own React Router library. I'll beging making a web as is and then separate the routing logic to make an installable package with it and publish it to NPM.
 
-- [ ] Create a way to make SPAs.
+- [x] Create a way to make SPAs.
 - [ ] Ability to navigate through pages using the back button.
 - [ ] Create a declarative "Link" component.
 - [ ] Create a declarative "Router" component.
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react'
+
+const NAVIGATION_EVENT = 'pushstate'
+
+function navigate(href) {
+  window.history.pushState({}, '', href) // Updates the url without reloading the page.
+  // Personalized event that makes the browser tell us when url changes:
+  const navigationEvent = new Event(NAVIGATION_EVENT) // There's no native way of listening for a forward navigation event (although there is for backwards navigation).
+  window.dispatchEvent(navigationEvent)
+}
 
 function HomePage() {
   return (
     <>
       <h1>Home</h1>
       <p>Example page to create a react router from scratch.</p>
-      <a href='/about'>About us</a>
+      <button onClick={() => navigate('/about')}>About us</button>
     </>
   )
 }
@@ -16,16 +24,33 @@ function AboutPage() {
     <>
       <h1>About</h1>
       <div>
-        <img src='/profilePic.webp' width='250px' alt='Andrés profile picture' />
+        <img
+          src='/profilePic.webp'
+          width='250px'
+          alt='Andrés profile picture'
+        />
         <p>Hi! I&apos;m Andrés, and I&apos;m creating a React Router clone.</p>
       </div>
-      <a href='/'>Home</a>
+      <button onClick={() => navigate('/')}>Home</button>
     </>
   )
 }
 
 function App() {
   const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname)
+    }
+
+    window.addEventListener(NAVIGATION_EVENT, onLocationChange)
+
+    return () => {
+      window.removeEventListener(NAVIGATION_EVENT, onLocationChange)
+    }
+  }, [])
+
   return (
     <>
       <main>
